467a1f95d8147d6753ee6f6a2489c11ca06454e4
IsaiasQB <isaias.qb@hotmail.com>
2022-04-23 16:28:47 -0400

    added methods to check for player's health

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -36,3 +36,28 @@ test('gets inventory from player or returns false', () => {
 
   expect(player.getInventory()).toEqual(false);
 });
+
+// create a test to get information about the player's health
+test("get's player's health value", () =>{
+  const player = new Player('Dave');
+  expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()))
+})
+
+test('checks if player is alive or not', () => {
+  const player = new Player('Dave');
+  expect(player.isAlive()).toBeTruthy();
+  player.health = 0;
+  expect(player.isAlive()).toBeFalsy();
+});
+
+// see if the correct amount of health is being subtracted from the Player health property
+test("substracts from player's health", () => {
+  const player = new Player('Dave');
+  const oldHealth = player.health;
+  
+  player.reduceHealth(5);
+  expect(player.health).toBe(oldHealth - 5);
+  
+  player.reduceHealth(99999);
+  expect(player.health).toBe(0);
+});
